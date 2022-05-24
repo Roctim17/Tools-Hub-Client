330d7ab90c26d0dbba359e9ad0c741ef5230f3de
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-05-25 01:18:23 +0600

    add require auth

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,23 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../firebase.init';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const logout = () => {
+        signOut(auth);
+        // localStorage.removeItem('accessToken');
+    }
+
     const menuItems = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About </Link></li>
         <li><Link to="/blog">blog</Link></li>
         <li><Link to="/reviews">Reviews</Link></li>
         <li><Link to="/contact">Contact Us </Link> </li>
-        <li><Link to="/login">Login </Link> </li>
+        <li>{user ? <button className="btn btn-ghost" onClick={logout}>Sign Out</button> : <Link to="/login">Login</Link>}</li>
     </>
     return (
         <div className="navbar bg-base-100">
